a2ac6de33b85c34cddb252e1e3e33832fdbcf22c
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-06-11 14:29:29 -0500

    configure nodemon and add lint to workflow

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,26 @@
 // Simple hello world entry point that waits for SIG INT to exit
 export default class App {
+  // eslint-disable-next-line no-undef
   private readonly _waitTimer: NodeJS.Timer
   private _isRunning: boolean
 
-  constructor() {
+  constructor () {
     this._waitTimer = setInterval(this.printWaitingMessage.bind(this), 10000)
     this.printWaitingMessage()
-    this._isRunning = true;
+    this._isRunning = true
   }
 
-  private printWaitingMessage() {
+  private printWaitingMessage () {
     console.log('Waiting for SIGINT...')
   }
 
-  public stop() {
+  public stop () {
     console.log('Goodbye world!')
     clearInterval(this._waitTimer)
     this._isRunning = false
   }
 
-  public get isRunning(): boolean {
+  public get isRunning (): boolean {
     return this._isRunning
   }
 }
